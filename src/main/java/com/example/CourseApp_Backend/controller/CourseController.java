4127aac22cbae4ce67a3db00e54ec7ac67371ced
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-06 11:10:31 +0530

    course added

--- src/main/java/com/example/CourseApp_Backend/controller/CourseController.java
+++ src/main/java/com/example/CourseApp_Backend/controller/CourseController.java
@@ -1,6 +1,9 @@
 package com.example.CourseApp_Backend.controller;
 
+import com.example.CourseApp_Backend.model.Course;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,15 @@ public class CourseController {
     public String Home()
     {
         return "Welcome";
+    }
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public String AddCourse(@RequestBody Course c)
+    {
+        System.out.println(c.getCourseTitle().toString());
+        System.out.println(c.getCourseDescription().toString());
+        System.out.println(c.getCourseVenue().toString());
+        System.out.println(c.getCourseDuration().toString());
+        System.out.println(c.getCourseDate().toString());
+        return "Course Added Successfully";
     }
 }
